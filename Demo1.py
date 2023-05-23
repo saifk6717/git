6237367b154876cb03adb9df428a6ee15f69ce9d
Saif Khan <114502837+saifk6717@users.noreply.github.com>
2023-05-23 11:41:50 +0530

    Update Demo1.py

--- Demo1.py
+++ Demo1.py
@@ -1,6 +1,9 @@
 x = "Hello"
 y = "World"
+z = "CDAC NOIDA"
 print("The input of x is",x)
 print("This is the inverse of x:",x[::-1])
 print("The input of y is",y)
 print("This is the second and third letter of y:",y[1:3])
+print("The input of z is",z)
+print("This is the inverse of z:",z[::-1])

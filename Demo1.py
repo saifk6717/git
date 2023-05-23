0bfe0563d4a57b59df439d7d9946b7a4a52d1664
saifk6717 <saifk6717@gmail.com>
2023-05-23 10:57:17 +0530

    Demo1.py

---
+++ Demo1.py
@@ -0,0 +1,6 @@
+x = "Hello"
+y = "World"
+print("The input of x is",x)
+print("This is the inverse of x:",x[::-1])
+print("The input of y is",y)
+print("This is the second and third letter of y:",y[1:3])
